2921df7bc0d6937806ce4b8a282f88a0ee1dd99e
Irina Styazhkina <58070479+iStyazhkina@users.noreply.github.com>
2021-01-24 16:16:30 +0700

    Update Resource.java

--- src/test/java/rest/Resource.java
+++ src/test/java/rest/Resource.java
@@ -3,11 +3,10 @@ package rest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.io.Serializable;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Resource implements Serializable {
+public class Resource {
 
     private List <ResourcesData> data;
 
