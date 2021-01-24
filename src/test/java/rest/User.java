092625fe5ae639419d28fd88501e8195238a8961
Irina Styazhkina <58070479+iStyazhkina@users.noreply.github.com>
2021-01-24 16:15:55 +0700

    Update User.java

--- src/test/java/rest/User.java
+++ src/test/java/rest/User.java
@@ -3,10 +3,8 @@ package rest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.io.Serializable;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class User implements Serializable {
+public class User {
 
      private UserData data;
 
